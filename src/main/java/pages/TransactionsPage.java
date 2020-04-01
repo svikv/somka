1b4f403b1918599af2735d60e20b8d3a83dd8cc6
viktor <saltison>
2020-04-01 20:23:07 +0300

    Added test for Transactions

---
+++ src/main/java/pages/TransactionsPage.java
@@ -0,0 +1,69 @@
+package pages;
+
+import libs.ActionWithWebElements;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class TransactionsPage {
+
+    private WebDriver webDriver;
+    private Wait<WebDriver> wait;
+    private Logger logger = Logger.getLogger(getClass());
+    private ActionWithWebElements actionWithWebElements;
+
+    private By transactionsTab = By.id("deal");
+    private By addButton = By.xpath("//div[@class='box-tools']");
+    private By dateSelector = By.id("deal_dealDate_date_day");
+    private By monthSelector = By.id("deal_dealDate_date_month");
+    private By yearSelector = By.id("deal_dealDate_date_year");
+    private By typeSelector = By.id("deal_dealType");
+    private By buyerSelector = By.id("deal_customer");
+    private By supplierSelector = By.id("deal_provider");
+    private By createButton = By.name("add");
+    private By table = By.xpath("//table[@id='device_list']//tbody//tr");
+    private By saveButton = By.name("save");
+
+    public TransactionsPage(WebDriver webDriver){
+        this.webDriver = webDriver;
+        actionWithWebElements = new ActionWithWebElements(webDriver);
+        PageFactory.initElements(this.webDriver, this);
+        wait = new WebDriverWait(webDriver, 1);
+    }
+
+    private void clickButton(By element){
+        actionWithWebElements.clickButton(element);
+    }
+
+    public void tableView(){
+        clickButton(transactionsTab);
+    }
+
+    public int countTableRaws(){
+        List<WebElement> rows = webDriver.findElements(table);
+        return rows.size();
+    }
+
+    public void addTableRecord(String date, String month, String year, String type, String buyer, String supplier){
+        clickButton(addButton);
+        actionWithWebElements.selectValueFromSelector(dateSelector, date);
+        actionWithWebElements.selectValueFromSelector(monthSelector, month);
+        actionWithWebElements.selectValueFromSelector(yearSelector, year);
+        actionWithWebElements.selectValueFromSelector(typeSelector,type);
+        actionWithWebElements.selectValueFromSelector(buyerSelector,buyer);
+        actionWithWebElements.selectValueFromSelector(supplierSelector,supplier);
+        clickButton(createButton);
+    }
+
+    public String getTableRecord (int index){
+        List<WebElement> rows = webDriver.findElements(table);
+        return rows.get(index).getText();
+    }
+}
+
---
+++ src/test/java/loginTests/TransactionsPageTest.java
@@ -0,0 +1,50 @@
+package loginTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+import pages.TransactionsPage;
+
+public class TransactionsPageTest {
+
+    private WebDriver webDriver = driverInit();
+    private String date = "05";
+    private String month = "марта";
+    private String year = "2020";
+    private String type = "Obmen";
+    private String buyer = "Sergey";
+    private String supplier = "EuroTruckService";
+
+    private WebDriver driverInit() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+    private LoginPage loginPage = new LoginPage(webDriver);
+    private TransactionsPage transactions = new TransactionsPage(webDriver);
+
+    @Test
+    public void addRecord() {
+        loginPage.loginToPage();
+        Assert.assertTrue(loginPage.isUserMenuNameDisplayed());
+
+        transactions.tableView();
+        int rawsBefore = transactions.countTableRaws();
+        transactions.addTableRecord(date, month, year, type, buyer, supplier);
+        int rawsAfter = transactions.countTableRaws();
+        Assert.assertEquals(rawsBefore + 1, rawsAfter);
+
+        String actualRecord = transactions.getTableRecord(rawsAfter - 1);
+        String expectedRecord = type + " " + buyer + " " + supplier;
+        Assert.assertTrue("Record wasn't added to table", actualRecord.contains(expectedRecord));
+    }
+
+    @After
+    public void tearDown(){
+        webDriver.quit();
+    }
+}
